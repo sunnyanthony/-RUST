f1613a0428f6412622073342464b16a9c98e748f
sunnyanthony <sunnyanthonycheng@gmail.com>
2022-10-05 01:05:24 +0800

    add testing

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1,5 +1,3 @@
-#![allow(unused)]
-fn main() {
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
@@ -21,17 +19,46 @@ fn example1(input: String) -> PyResult<String> {
 fn example2(value: i32, name: String) -> Py<ExampleClass> {
     let gil = Python::acquire_gil();
     let py = gil.python();
-	Py::new(py, ExampleClass { num: value, name: name }).unwrap()
+    Py::new(
+        py,
+        ExampleClass {
+            num: value,
+            name: name,
+        },
+    )
+    .unwrap()
 }
 
 /// A Python module implemented in Rust.
+#[allow(unused)]
 #[pymodule]
 fn rust_python(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(example1, m)?)?;
     m.add_function(wrap_pyfunction!(example2, m)?)?;
-	m.add_class::<ExampleClass>()?;
+    m.add_class::<ExampleClass>()?;
 
     Ok(())
 }
-}
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_example1() {
+        use pyo3::types::IntoPyDict;
+        let gil = Python::acquire_gil();
+        let py = gil.python();
+        let m = PyModule::new(py, "python_test").unwrap();
+        m.add_wrapped(wrap_pyfunction!(example1)).unwrap();
+        let locals = [("python_test", m)].into_py_dict(py);
+        py.run(
+            r#"
+            assert python_test.example1("me") == "You are me"
+            "#,
+            None,
+            Some(locals),
+        )
+        .unwrap();
+    }
+}
